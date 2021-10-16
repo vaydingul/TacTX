a26cd9202f4e393da70bca2259e9b4b2ad5ee018
Volkan Aydıngül <volkanaydingul@gmail.com>
2021-10-16 15:19:51 +0300

    Update DataReaderWriter.m and ForceSensorReader.m

---
+++ src/DataReaderWriter.m
@@ -0,0 +1,34 @@
+classdef DataReaderWriter < DataHandler
+
+    properties (Access = public)
+
+        Data
+        SampleRate
+
+    end
+
+    methods (Access = public)
+
+        function initialize(obj)
+
+        end
+
+        function read(obj)
+
+        end
+
+        function write(obj)
+
+        end
+
+        function readwrite(obj)
+
+        end
+
+        function kill(obj)
+
+        end
+
+    end
+
+end
---
+++ src/ForceSensorReader.m
@@ -0,0 +1,34 @@
+classdef ForceSensorReader < DataReader
+	
+	properties (Access = public)
+		
+		Data
+		SampleRate
+
+	end
+
+	methods (Access = public)
+		
+		function initialize(obj)
+			
+        end
+
+        function read(obj)
+
+        end
+
+        function write(obj)
+
+        end
+
+        function readwrite(obj)
+
+        end
+
+        function kill(obj)
+
+        end
+		
+	end
+
+end
