9aa1c865de5506474a46e6e1a3aff603c01cc302
Volkan Aydıngül <volkanaydingul@gmail.com>
2021-11-04 14:04:49 +0300

    Bug fixes

--- experiments/04.11.2021_Birthday_Joy/Special_Waves_Amplitude_Change/Square/generateConfigFile.m
+++ experiments/04.11.2021_Birthday_Joy/Special_Waves_Amplitude_Change/Square/generateConfigFile.m
@@ -93,19 +93,16 @@ function signal = generateSignal(signalType, time, frequency, amplitude)
 
         case 1
 
-            signal = [zeros(floor(length(time) / 2), 1); ...
-                    amplitude * sin(2 * pi * frequency * time(1:floor(length(time) / 2)))'];
+            signal = amplitude * sin(2 * pi * frequency * time(1:length(time)))';
 
         case 2
 
-            signal = [zeros(floor(length(time) / 2), 1); ...
-                    amplitude * sawtooth(2 * pi * frequency * time(1:floor(length(time) / 2)))'];
+            signal = amplitude * sawtooth(2 * pi * frequency * time(1:length(time)))';
 
         case 3
 
-            signal = [zeros(floor(length(time) / 2), 1); ...
-                    amplitude * square(2 * pi * frequency * time(1:floor(length(time) / 2)))'];
-
+            signal = amplitude * square(2 * pi * frequency * time(1:length(time)))';
+            
         otherwise
 
             error("Please input correct signal type!");
