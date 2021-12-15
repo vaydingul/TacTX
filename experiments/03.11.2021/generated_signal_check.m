d71c65c23bf47828eea678c32b39099cbbb34c98
Volkan-NewSetup <vaydingul20@ku.edu.tr>
2021-12-15 16:44:49 +0300

    New sine wave experiment is added

--- experiments/03.11.2021/generated_signal_check.m
+++ experiments/03.11.2021/generated_signal_check.m
@@ -1,5 +1,5 @@
 initExperiment;
-config = generateConfigFile(CONFIG, 1, 1 , 0.5, 4);
+config = generateConfigFile(CONFIG, 1, 4 , 0.5, 4);
 tactx = TacTX('State', IdleState, 'Config', config);
 
 tactx.SignalGenerator.Signal = tactx.Config.SIGNAL{1};
