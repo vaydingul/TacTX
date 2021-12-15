b4ae7779f14ad61db0791d0b32617e6a9bfb3bbd
Volkan-NewSetup <vaydingul20@ku.edu.tr>
2021-12-15 15:00:55 +0300

    Sine wave experiment

--- experiments/03.11.2021/generated_signal_check.m
+++ experiments/03.11.2021/generated_signal_check.m
@@ -1,5 +1,5 @@
 initExperiment;
-config = generateConfigFile(CONFIG, 1, 3 , 0.5, 4);
+config = generateConfigFile(CONFIG, 1, 1 , 0.5, 4);
 tactx = TacTX('State', IdleState, 'Config', config);
 
 tactx.SignalGenerator.Signal = tactx.Config.SIGNAL{1};
@@ -8,17 +8,13 @@ tactx.SignalGenerator.Signal = tactx.Config.SIGNAL{1};
 disp("NOW!");
 tactx.run("RepeatOutput");
 
+%%
+tactx.idle();
 
-%tactx.idle();
-%
-%figure;
-%plot(tactx.ForceSensor.ForceTorque);
-%
-%figure;
-%plot(tactx.Accelerometer.Acceleration);
-%
-%figure;
-%scatter(tactx.FingerTracker.FingerPosition(:,1),tactx.FingerTracker.FingerPosition(:,2))
-%
-%figure;
-%plot(sgn);
+figure;
+plot(tactx.ForceSensor.ForceTorque);
+
+figure;
+plot(tactx.Accelerometer.Acceleration);
+
+
